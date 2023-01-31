da255f462b5f181e0b101bf37125e1c55c3699bf
Lico-Tom <583566138@qq.com>
2023-01-31 14:44:19 +0800

    add database switch environment variables

--- docker-build/scripts/start-kong.sh
+++ docker-build/scripts/start-kong.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 CONF=/usr/local/kong/kong.conf
-echo 'database = off' > $CONF
+if [ ! -n "${DATABASE_SWITCH}" ]; then
+    echo 'database = off' > $CONF
+else
+    echo 'database = '${DATABASE_SWITCH}'' > $CONF
+fi
+
 kong start --conf $CONF
